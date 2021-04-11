be79c266b4b53ba14b45656c6db8d72c815cd423
amoizhao <46307589+amoizhao@users.noreply.github.com>
2021-04-10 18:02:03 -0700

    -_-

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,24 @@
 package _01_File_Recorder;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String args[]) {
+		try {
+			System.out.println("Type a message: ");
+			Scanner scanner = new Scanner(System.in);
+
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/frTest.txt", true);
+			fw.write(scanner.nextLine());
+			fw.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileDecryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileDecryptor.java
@@ -1,16 +1,27 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
 public class FileDecryptor {
 	/*
 	 * Decryption is the process of taking encoded or encrypted text or other data
-	 * and converting it back into text that you or the computer can read and understand.
+	 * and converting it back into text that you or the computer can read and
+	 * understand.
 	 *
-	 * The shift cipher is decrypted by using using the key and shifting back up,
-	 * at the end of our encryption example we had our alphabet as:
+	 * The shift cipher is decrypted by using using the key and shifting back up, at
+	 * the end of our encryption example we had our alphabet as:
 	 *
 	 * e f g h i j k l m n o p q r s t u v w x y z a b c d
 	 *
-	 * If we shift it back up by 4 based on the key we used the alphabet is decrypted:
+	 * If we shift it back up by 4 based on the key we used the alphabet is
+	 * decrypted:
 	 *
 	 * a b c d e f g h i j k l m n o p q r s t u v w x y z
 	 *
@@ -19,4 +30,25 @@ public class FileDecryptor {
 	 * Create a program that opens the file created by FileEncryptor and decrypts
 	 * the message, then display it to the user in a JOptionPane.
 	 */
+	public static void main(String args[]) {
+		int key = 1;
+		String newString = "";
+
+		try {
+			BufferedReader br = new BufferedReader(new FileReader("src/_02_File_Encrypt_Decrypt/enTest.txt"));
+			String input = br.readLine();
+			for (int i = 0; i < input.length(); i++) {
+				newString += (char) (input.charAt(i) - key);
+			}
+			JOptionPane.showMessageDialog(null, newString);
+			br.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
 }
